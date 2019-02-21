81cba8afc7af9e9de8d4cc4ccf8121dc0f944703
ShivamBharadwaj31 <shivam.bhardwag_mca18@gla.ac.in>
2019-02-21 11:44:48 +0530

    demo

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hello Shivam", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This is demo", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
